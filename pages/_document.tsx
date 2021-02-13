55509d2cd30af4c8964142c66169b1b9a0c39ff0
Woojo Kim <kwoojo0127@gmail.com>
2021-02-13 15:00:20 +0900

    Add _document.tsx for server-side-rendering

---
+++ pages/_document.tsx
@@ -0,0 +1,61 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ReactElement } from 'react';
+import { resetServerContext } from 'react-beautiful-dnd';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<{
+    styles: JSX.Element;
+    html: string;
+    head?: (JSX.Element | null)[] | undefined;
+  }> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    resetServerContext();
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { ReactElement } from 'react';
 
+import Draggable from '../components/example/Draggable';
+
 const Home = (): ReactElement => {
   return (
     <>
       <h1>Hello Next</h1>
+      <Draggable />
     </>
   );
 };
